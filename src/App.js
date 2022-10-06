097a490d12f215ba9046fab63131a9cf1e86fa1f
MargaritaVin <ritasergienko@rambler.ru>
2022-10-06 16:55:40 +0300

    Title component v2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+
 import Title from "./components/Title";
 
 function App() {
-  return (
-    <div>
-      <Title name="Products" type="bold"/>
-      <Title name="Employees" type="normal"/>
-    </div>
-  );
+  const [isOpened, setOpened] = useState(false);
+
+  const handleClick = () => {
+    setOpened(true);
+  };
+
+  if(!isOpened) {
+    return <button onClick={handleClick}>Open title</button>;
+  }
+
+  return <Title name="You opened title" type="normal"/>;
+  
+  // return (
+  //   <div>
+  //     <Title name="Products" type="bold"/>
+  //     <Title name="Employees" type="normal"/>
+  //   </div>
+  // );
 }
 
 export default App;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,16 @@
+import PropTypes from 'prop-types';
+
 const Title = ({name, type}) => {
-    return <p style={{ fontWeight: type }}> {name}</p>
+    return <p style={{ fontWeight: type }}>{name}</p>
+};
+
+Title.propTypes = {
+    name: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(["bold", "normal"]),
+};
+
+Title.defaultProps = {
+    type: "normal",
 };
 
 export default Title;
